797b93b95df1ab6eb4c1adc08b1a4302dd24b31e
Jose Vargas <ruedagato@gmail.com>
2020-05-13 21:43:32 -0500

    add spiner and material

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import {
+  FormGroup,
+  FormBuilder,
+  Validators,
+  AbstractControl,
+} from '@angular/forms';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { Router } from '@angular/router';
 
@@ -13,6 +18,8 @@ export class LoginComponent implements OnInit {
   constador = 10;
   error = '';
 
+  isLoad = false;
+
   constructor(
     private formBuilder: FormBuilder,
     private afAuth: AngularFireAuth,
@@ -21,21 +28,36 @@ export class LoginComponent implements OnInit {
 
   ngOnInit(): void {
     this.loginForm = this.formBuilder.group({
-      email: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
       pass: ['', [Validators.required, Validators.minLength(6)]],
     });
   }
 
   goToLogin() {
+    this.isLoad = true;
     this.error = '';
     const { pass, email } = this.loginForm.value;
     this.afAuth
       .signInWithEmailAndPassword(email, pass)
       .then((user) => {
         this.router.navigate(['home']);
+        this.isLoad = false;
       })
       .catch(({ message }) => {
         this.error = message;
+        this.isLoad = false;
       });
+  }
+
+  getError(control: AbstractControl) {
+    if (control.hasError('email')) {
+      return 'Email invalid';
+    } else if (control.hasError('required')) {
+      return 'This field is required';
+    } else if (control.hasError('minlength')) {
+      return `The min character of the password is ${control.errors.minlength.requiredLength}`;
+    } else {
+      return 'Any error';
+    }
   }
 }
