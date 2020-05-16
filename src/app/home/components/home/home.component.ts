7c73bccc84d75013b7f3171bd0dc11b27f1ae71b
Jose Vargas <ruedagato@gmail.com>
2020-05-16 17:50:35 -0500

    Create modal to add product

--- src/app/home/components/home/home.component.ts
+++ src/app/home/components/home/home.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { AddProductModalComponent } from '../add-product-modal/add-product-modal.component';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  constructor(private dialog: MatDialog) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  openDialogAddProduct() {
+    this.dialog.open(AddProductModalComponent);
   }
-
 }
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -8,15 +8,18 @@ import { HomeComponent } from './components/home/home.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { AddProductModalComponent } from './components/add-product-modal/add-product-modal.component';
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, AddProductModalComponent],
   imports: [
     CommonModule,
     HomeRoutingModule,
     MatToolbarModule,
     MatIconModule,
     MatButtonModule,
+    MatDialogModule,
   ],
 })
 export class HomeModule {}
