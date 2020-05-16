efc72e3fe0e2017566bc61a6cfd0a83bf01f30af
Jose Vargas <ruedagato@gmail.com>
2020-05-16 12:41:56 -0500

    change registe style

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,7 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import {
+  FormGroup,
+  FormBuilder,
+  Validators,
+  AbstractControl,
+} from '@angular/forms';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { Router } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-register',
@@ -11,6 +17,8 @@ import { Router } from '@angular/router';
 export class RegisterComponent implements OnInit {
   registerForm: FormGroup;
   error = '';
+  loginPath = `/login`;
+  load = false;
 
   constructor(
     formBuilder: FormBuilder,
@@ -18,6 +26,7 @@ export class RegisterComponent implements OnInit {
     private router: Router
   ) {
     this.registerForm = formBuilder.group({
+      name: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(6)]],
     });
@@ -26,11 +35,31 @@ export class RegisterComponent implements OnInit {
   ngOnInit(): void {}
 
   registerUser() {
+    this.load = true;
     this.error = '';
-    const { email, password } = this.registerForm.value;
+    const { email, password, name } = this.registerForm.value;
     this.afAuth
       .createUserWithEmailAndPassword(email, password)
-      .then(() => this.router.navigate(['home']))
-      .catch(({ message }) => (this.error = message));
+      .then(async (usr) => {
+        await usr.user.updateProfile({ displayName: name });
+        this.load = false;
+        this.router.navigate(['home']);
+      })
+      .catch(({ message }) => {
+        this.load = false;
+        this.error = message;
+      });
+  }
+
+  getError(control: AbstractControl) {
+    if (control.hasError('email')) {
+      return 'Email invalid';
+    } else if (control.hasError('required')) {
+      return 'This field is required';
+    } else if (control.hasError('minlength')) {
+      return `The min character of the password is ${control.errors.minlength.requiredLength}`;
+    } else {
+      return 'Any error';
+    }
   }
 }
