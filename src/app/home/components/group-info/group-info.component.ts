6b06ab0c923312e5aade5009d2a84fe60fb22784
Jose Vargas <ruedagato@gmail.com>
2020-06-14 13:32:30 -0500

    group cpmlete

--- src/app/home/components/group-info/group-info.component.ts
+++ src/app/home/components/group-info/group-info.component.ts
@@ -6,6 +6,7 @@ import { GroupModel } from '../../../core/models/group.model';
 import { Subscription } from 'rxjs';
 import { MatDialog } from '@angular/material/dialog';
 import { AddProductModalComponent } from '../add-product-modal/add-product-modal.component';
+import { ProductModel } from 'src/app/core/models/product.model';
 
 @Component({
   selector: 'app-group-info',
@@ -44,4 +45,28 @@ export class GroupInfoComponent implements OnInit, OnDestroy {
   }
 
   ngOnInit(): void {}
+
+  async addElement(product: ProductModel) {
+    const ref = this.db.database.ref(
+      `group/${this.group.uid}/products/${product.uid}`
+    );
+    await ref.transaction((productUpdate: ProductModel) => {
+      if (!!productUpdate) {
+        productUpdate.quantity++;
+      }
+      return productUpdate;
+    });
+  }
+
+  async removeElement(product: ProductModel) {
+    const ref = this.db.database.ref(
+      `group/${this.group.uid}/products/${product.uid}`
+    );
+    await ref.transaction((productUpdate: ProductModel) => {
+      if (!!productUpdate && productUpdate.quantity > 0) {
+        productUpdate.quantity--;
+      }
+      return productUpdate;
+    });
+  }
 }
